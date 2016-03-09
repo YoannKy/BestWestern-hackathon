732a06206c567db4b583a6b79521c07847cb8ac0
yoannKy <y.keoxay@gmail.com>
2016-03-09 18:58:21 +0100

    fixed style

--- app/Http/Controllers/Auth/SessionController.php
+++ app/Http/Controllers/Auth/SessionController.php
@@ -2,75 +2,70 @@
 
 namespace App\Http\Controllers\Auth;
 
-use Sentinel;
+use App\Http\Controllers\Controller;
 use Centaur\AuthManager;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use Centaur\Dispatches\BaseDispatch;
 
-class SessionController extends Controller
-{
-    /** @var Centaur\AuthManager */
-    protected $authManager;
+class SessionController extends Controller {
+	/** @var Centaur\AuthManager */
+	protected $authManager;
 
-    /**
-     * Create a new authentication controller instance.
-     *
-     * @return void
-     */
-    public function __construct(AuthManager $authManager)
-    {
-        $this->middleware('sentinel.guest', ['except' => 'getLogout']);
-        $this->authManager = $authManager;
-    }
+	/**
+	 * Create a new authentication controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct(AuthManager $authManager) {
+		$this->middleware('sentinel.guest', ['except' => 'getLogout']);
+		$this->authManager = $authManager;
+	}
 
-    /**
-     * Show the Login Form
-     * @return View
-     */
-    public function getLogin()
-    {
-        return view('Centaur::auth.login');
-    }
+	/**
+	 * Show the Login Form
+	 * @return View
+	 */
+	public function getLogin() {
+		return view('Centaur::auth.login');
+	}
 
-    /**
-     * Handle a Login Request
-     * @return Response|Redirect
-     */
-    public function postLogin(Request $request)
-    {
-        // Validate the Form Data
-        $result = $this->validate($request, [
-            'email' => 'required',
-            'password' => 'required'
-        ]);
+	/**
+	 * Handle a Login Request
+	 * @return Response|Redirect
+	 */
+	public function postLogin(Request $request) {
+		// Validate the Form Data
+		$result = $this->validate($request, [
+			'email' => 'required',
+			'pseudo' => 'required',
+			'password' => 'required',
+		]);
 
-        // Assemble Login Credentials
-        $credentials = [
-            'email' => trim($request->get('email')),
-            'password' => $request->get('password'),
-        ];
-        $remember = (bool)$request->get('remember', false);
+		// Assemble Login Credentials
+		$credentials = [
+			'pseudo' => trim($request->get('pseudo')),
+			'email' => trim($request->get('email')),
+			'password' => $request->get('password'),
+		];
+		$remember = (bool) $request->get('remember', false);
 
-        // Attempt the Login
-        $result = $this->authManager->authenticate($credentials, $remember);
+		// Attempt the Login
+		$result = $this->authManager->authenticate($credentials, $remember);
 
-        // Return the appropriate response
-        $path = session()->pull('url.intended', route('dashboard'));
-        return $result->dispatch($path);
-    }
+		// Return the appropriate response
+		$path = session()->pull('url.intended', route('dashboard'));
+		return $result->dispatch($path);
+	}
 
-    /**
-     * Handle a Logout Request
-     * @return Response|Redirect
-     */
-    public function getLogout(Request $request)
-    {
-        // Terminate the user's current session.  Passing true as the
-        // second parameter kills all of the user's active sessions.
-        $result = $this->authManager->logout(null, null);
+	/**
+	 * Handle a Logout Request
+	 * @return Response|Redirect
+	 */
+	public function getLogout(Request $request) {
+		// Terminate the user's current session.  Passing true as the
+		// second parameter kills all of the user's active sessions.
+		$result = $this->authManager->logout(null, null);
 
-        // Return the appropriate response
-        return $result->dispatch(route('dashboard'));
-    }
+		// Return the appropriate response
+		return $result->dispatch(route('dashboard'));
+	}
 }
