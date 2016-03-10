a9525508165058a9f1a463712234b7f66ac46bd9
Aberish <albert.nguyen.pro14@gmail.com>
2016-03-10 11:45:35 +0100

    add map in laravel

---
+++ app/Http/Controllers/HostelController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alberish
+ * Date: 10/03/16
+ * Time: 10:50
+ */
+
+namespace App\Http\Controllers;
+
+use App\Models\Hostel;
+
+class HostelController extends Controller
+{
+    public function index() {
+        $hostels = Hostel::all();
+
+        return view('map', ['hostels' => $hostels, 'count' => 1]);
+    }
+
+}
+
