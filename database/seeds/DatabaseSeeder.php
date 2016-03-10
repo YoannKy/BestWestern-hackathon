cffe45a855aa27f1447ba2432f55c54068e10d94
yoannKy <y.keoxay@gmail.com>
2016-03-10 13:47:58 +0100

    fixed seeder for sentinel

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -9,6 +9,12 @@ class DatabaseSeeder extends Seeder {
 	 * @return void
 	 */
 	public function run() {
+		//disable foreign key check for this connection before running seeders
+		DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
 		$this->call('HostelsTableSeeder');
+		$this->call('SentinelDatabaseSeeder');
+
+		DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 	}
 }
